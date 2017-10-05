747d91d65ebc919997bc33a052f007dec9b96c4d
JemimaEllis <jemimaeellis@gmail.com>
2017-10-05 09:30:11 +0100

    Comments not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,37 @@
 Rails.application.routes.draw do
+  # get 'comments/index'
+
+  # get 'comments/show'
+
+  # get 'comments/new'
+
+  # get 'comments/create'
+
+  # get 'comments/edit'
+
+  # get 'comments/update'
+
+  # get 'comments/destroy'
+
+  # get 'posts/index'
+
+  # get 'posts/show'
+
+  # get 'posts/new'
+
+  # get 'posts/create'
+
+  # get 'posts/edit'
+
+  # get 'posts/update'
+
+  # get 'posts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+p1 = Post.create!(title: "How to eat on a student budget", author: "Amelia Smith", theme: "Food", picture_url: "http://www.easystudentmeals.co.uk/13.jpg", body: "When going to University, no longer having your meals miracously appear when your parents call 'dinner' can be a change. You may start ordering takeaways every night, but after the first week you may then realise you have used up the entirity of your months allowance. Equily you may just start cooking beans on toast every night, but soon you may start to notice the health effects of your un-balanced diet, or you may simply just be so bored of beans on toast that even looking at them makes you feel ill. Well don't worry, these are all normal reactions to living away from home for the first time. So this is a blog with easy, cheap, and balanced meals to help you eat well at university.")
+
+c1 = Comment.create!(username: "cheekymonkey123", body: "This blog saved my life when I went to University", location: "123 Carton lane, Surrey, R5 6HT", email_address: "cheekymonkey123@hotmail.co.uk", age: 19, post_id: p1.id)
+c2 = Comment.create!(username: "foodie", body: "Really liked the recipes, great mix of different ingredients", location: "Pocock street, London, SE1 0BW", email_address: "moodiefoodie@gmail.com", age: 20, post_id: p1.id)
+
+puts "Finished seeding..."
+
